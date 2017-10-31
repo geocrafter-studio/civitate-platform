bbe0ff7eb134af544dc5b7e8a719a43f744ac135
Fernando Ribeiro <fernandinand@gmail.com>
2017-10-31 16:19:41 +0000

    Modify startup scripts for production containers

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# npm run build
-# python src/manage.py collectstatic --noinput
+npm install
+npm run build
+python src/manage.py collectstatic --noinput
 python src/manage.py compilemessages
-gunicorn wsgi:application -w 3 -b 0.0.0.0:${PORT} --log-level=debug
+npm run start-prod
+#gunicorn wsgi:application -w 3 -b 0.0.0.0:${PORT} --log-level=debug
