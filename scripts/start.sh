823e5d6945a6c032fd3e7c5268867d95422d0c24
Fernando Ribeiro <fernandinand@gmail.com>
2017-11-02 15:10:35 +0000

    Several fixes on container startup configs

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-npm install
-npm run build
+# Export environment variables
+export $(cat /platform/src/.env | xargs)
+# Collect static
 python src/manage.py collectstatic --noinput
-python src/manage.py compilemessages
+# Compile translations
+cd /platform/src/flavors/${FLAVOR}
+python /platform/src/manage.py compilemessages
+# Start app in production mode
 npm run start-prod
-#gunicorn wsgi:application -w 3 -b 0.0.0.0:${PORT} --log-level=debug
